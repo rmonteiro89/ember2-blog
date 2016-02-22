f7aa28f1b8095021ee05f4c518a8c7d1ced8fa8a
Raphael Monteiro <rmonteiro89@hotmail.com>
2016-02-22 23:35:56 +0100

    Tutorial ember done

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'ember2-blog',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://rmonteiro-ember-blog2.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
